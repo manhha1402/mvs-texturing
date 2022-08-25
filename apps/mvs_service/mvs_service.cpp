e3955095b614331923250d07f626c92ebba5ae5b
Manh-Ha Hoang <manhha1402@gmail.com>
2022-08-25 21:07:19 +0700

    update service

--- apps/mvs_service/mvs_service.cpp
+++ apps/mvs_service/mvs_service.cpp
@@ -1,37 +1,35 @@
 
 
-#include <iostream>
 #include <fstream>
-#include <vector>
+#include <iostream>
 #include <tbb/task_scheduler_init.h>
+#include <vector>
 
-#include <util/timer.h>
-#include <util/system.h>
-#include <util/file_system.h>
-#include <mve/mesh_io_ply.h>
-#include <mve/image_tools.h>
-#include <mve/image_io.h>
-#include <mve/mesh.h>
-#include "tex/util.h"
-#include "tex/timer.h"
+#include "arguments.h"
+#include "neura_vision_tools_msgs/TextureMapping.h"
 #include "tex/debug.h"
-#include "tex/texturing.h"
 #include "tex/progress_counter.h"
+#include "tex/texturing.h"
+#include "tex/timer.h"
+#include "tex/util.h"
+#include <mve/image_io.h>
+#include <mve/image_tools.h>
+#include <mve/mesh.h>
+#include <mve/mesh_io_ply.h>
 #include <open3d/Open3D.h>
 #include <opencv2/opencv.hpp>
-#include "neura_vision_tools_msgs/TextureMapping.h"
-#include <ros/ros.h>
 #include <ros/package.h>
-#include "arguments.h"
+#include <ros/ros.h>
+#include <util/file_system.h>
+#include <util/system.h>
+#include <util/timer.h>
 
 void readData(const std::string data_path,
-              std::vector<tex::TextureView>& texture_views)
-{
+              std::vector<tex::TextureView> &texture_views) {
 
   std::string scene_dir = data_path + "/texture/scene/";
 
-
-  std::string const tmp_dir = data_path+"/tmp_dir/";
+  std::string const tmp_dir = data_path + "/tmp_dir/";
   if (open3d::utility::filesystem::DirectoryExists(tmp_dir)) {
     open3d::utility::filesystem::DeleteDirectory(tmp_dir);
     open3d::utility::filesystem::MakeDirectory(tmp_dir);
@@ -39,19 +37,15 @@ void readData(const std::string data_path,
     open3d::utility::filesystem::MakeDirectory(tmp_dir);
   }
 
-
   std::cout << "Generating texture views: " << std::endl;
   tex::generateTextureViews(scene_dir, texture_views);
-
 }
 
-
-class MVSTexService
-{
+class MVSTexService {
 public:
-  MVSTexService(ros::NodeHandle& node);
-  bool doTextureMapping(neura_vision_tools_msgs::TextureMappingRequest& req,
-                        neura_vision_tools_msgs::TextureMappingResponse& res);
+  MVSTexService(ros::NodeHandle &node);
+  bool doTextureMapping(neura_vision_tools_msgs::TextureMappingRequest &req,
+                        neura_vision_tools_msgs::TextureMappingResponse &res);
 
   virtual ~MVSTexService();
 
@@ -59,22 +53,21 @@ private:
   ros::NodeHandle pnh_;
   ros::ServiceServer tex_map_srv_;
 };
-MVSTexService::MVSTexService(ros::NodeHandle& node) : pnh_(node)
-{
-  tex_map_srv_ = pnh_.advertiseService("mvs_service", &MVSTexService::doTextureMapping, this);
+MVSTexService::MVSTexService(ros::NodeHandle &node) : pnh_(node) {
+  tex_map_srv_ = pnh_.advertiseService("mvs_service",
+                                       &MVSTexService::doTextureMapping, this);
 }
-MVSTexService::~MVSTexService()
-{
-}
+MVSTexService::~MVSTexService() {}
 
-bool MVSTexService::doTextureMapping(neura_vision_tools_msgs::TextureMappingRequest& req,
-                      neura_vision_tools_msgs::TextureMappingResponse& res)
-{
+bool MVSTexService::doTextureMapping(
+    neura_vision_tools_msgs::TextureMappingRequest &req,
+    neura_vision_tools_msgs::TextureMappingResponse &res) {
   mve::TriangleMesh::Ptr mesh;
   std::vector<tex::TextureView> texture_views;
-  readData(req.data_path,texture_views);
+  readData(req.scan_client_info.data_path, texture_views);
   std::cout << "Load and prepare mesh: " << std::endl;
-  std::string mesh_file = req.data_path + "/" + req.ply_name_file;
+  std::string mesh_file = req.scan_client_info.data_path + "/" +
+                          req.scan_client_info.object_name + ".ply";
   mesh = mve::geom::load_ply_mesh(mesh_file);
   std::size_t const num_faces = mesh->get_faces().size() / 3;
   mve::MeshInfo mesh_info(mesh);
@@ -85,25 +78,24 @@ bool MVSTexService::doTextureMapping(neura_vision_tools_msgs::TextureMappingRequ
   std::cout << "View selection:" << std::endl;
   util::WallTimer rwtimer;
   Arguments conf;
-  conf.in_scene = req.data_path + "/texture/scene/";
+  conf.in_scene = req.scan_client_info.data_path + "/texture/scene/";
   conf.in_mesh = mesh_file;
   std::string obj_file;
-  if (req.object_name.empty())
-  {
-    obj_file = req.ply_name_file.substr(0, req.ply_name_file.length() - 3);
+  if (req.scan_client_info.object_name.empty()) {
+    obj_file = "texture_mesh.obj";
+  } else {
+    obj_file = req.scan_client_info.object_name;
   }
-  else{
-    obj_file = req.object_name;
-  }
-  if (req.output_path.empty()) req.output_path = req.data_path;
+  if (req.output_path.empty())
+    req.output_path = req.scan_client_info.data_path;
   // Create a folder inside output path
-  std::string object_model_dir = req.output_path + "/" + req.object_name + "/";
-  if (!open3d::utility::filesystem::DirectoryExists(object_model_dir))
-  {
+  std::string object_model_dir =
+      req.output_path + "/" + req.scan_client_info.object_name + "/";
+  if (!open3d::utility::filesystem::DirectoryExists(object_model_dir)) {
     open3d::utility::filesystem::MakeDirectoryHierarchy(object_model_dir);
   }
 
-  conf.out_prefix = object_model_dir + req.object_name;
+  conf.out_prefix = object_model_dir + req.scan_client_info.object_name;
   conf.data_cost_file = "";
   conf.labeling_file = "";
   conf.write_timings = false;
@@ -119,13 +111,17 @@ bool MVSTexService::doTextureMapping(neura_vision_tools_msgs::TextureMappingRequ
   tex::VertexProjectionInfos vertex_projection_infos;
   std::cout << "Generating texture patches:" << std::endl;
   tex::generate_texture_patches(graph, mesh, mesh_info, &texture_views,
-      conf.settings, &vertex_projection_infos, &texture_patches);
+                                conf.settings, &vertex_projection_infos,
+                                &texture_patches);
   std::cout << "Running global seam leveling:" << std::endl;
-  tex::global_seam_leveling(graph, mesh, mesh_info, vertex_projection_infos, &texture_patches);
+  tex::global_seam_leveling(graph, mesh, mesh_info, vertex_projection_infos,
+                            &texture_patches);
   std::cout << "Running local seam leveling:" << std::endl;
-  tex::local_seam_leveling(graph, mesh, vertex_projection_infos, &texture_patches);
+  tex::local_seam_leveling(graph, mesh, vertex_projection_infos,
+                           &texture_patches);
   std::cout << "Generating texture atlases:" << std::endl;
-  tex::generate_texture_atlases(&texture_patches, conf.settings, &texture_atlases);
+  tex::generate_texture_atlases(&texture_patches, conf.settings,
+                                &texture_atlases);
   std::cout << "Building objmodel:" << std::endl;
   tex::Model model;
   tex::build_model(mesh, texture_atlases, &model);
@@ -138,16 +134,12 @@ bool MVSTexService::doTextureMapping(neura_vision_tools_msgs::TextureMappingRequ
   return true;
 }
 
-
 int main(int argc, char **argv) {
 
-   ros::init(argc, argv, "mvs_service_node");
-   ros::NodeHandle node;
-   std::shared_ptr<MVSTexService> tex_map = std::make_shared<MVSTexService>(node);
-   ROS_INFO("MVS service is ready, wait for client");
-   ros::spin();
- 
-
+  ros::init(argc, argv, "mvs_service_node");
+  ros::NodeHandle node;
+  std::shared_ptr<MVSTexService> tex_map =
+      std::make_shared<MVSTexService>(node);
+  ROS_INFO("MVS service is ready, wait for client");
+  ros::spin();
 }
-
-
